a42009b7f9e469c89d934f75e98b189e044645e2
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-02 02:58:01 +0700

    Update dependencies
    Fixes #1

--- src/run_cairo.rs
+++ src/run_cairo.rs
@@ -1,25 +1,23 @@
 //! Compiles and runs a Cairo program.
-
 use std::path::Path;
 
 use anyhow::{Context, Ok};
-use compiler::db::RootDatabase;
-use compiler::diagnostics::check_and_eprint_diagnostics;
-use compiler::project::setup_project;
-use diagnostics::ToOption;
-use runner::SierraCasmRunner;
-use sierra_generator::db::SierraGenGroup;
-use sierra_generator::replace_ids::replace_sierra_ids_in_program;
+use cairo_compiler::db::RootDatabase;
+use cairo_compiler::diagnostics::check_and_eprint_diagnostics;
+use cairo_compiler::project::setup_project;
+use cairo_diagnostics::ToOption;
+use cairo_runner::SierraCasmRunner;
+use cairo_sierra_generator::db::SierraGenGroup;
+use cairo_sierra_generator::replace_ids::replace_sierra_ids_in_program;
 
-pub fn cairo_runner(path: &str) -> anyhow::Result<String> {
-    let available_gas = Some(0);
+pub fn run_cairo(path: &str) -> anyhow::Result<String> {
     let mut db_val = RootDatabase::default();
     let db = &mut db_val;
 
     let main_crate_ids = setup_project(db, Path::new(&path))?;
 
     if check_and_eprint_diagnostics(db) {
-        anyhow::bail!("failed to compile: {}", &path);
+        anyhow::bail!("failed to compile: {}", path);
     }
 
     let sierra_program = db
@@ -29,18 +27,17 @@ pub fn cairo_runner(path: &str) -> anyhow::Result<String> {
     let runner = SierraCasmRunner::new(replace_sierra_ids_in_program(db, &sierra_program), false)
         .with_context(|| "Failed setting up runner.")?;
     let result = runner
-        .run_function("::main", &[], &available_gas)
+        .run_function("::main", &[], None)
         .with_context(|| "Failed to run the function.")?;
     let mut result_str = String::new();
-
     match result.value {
-        runner::RunResultValue::Success(values) => {
+        cairo_runner::RunResultValue::Success(values) => {
             for val in values.iter() {
                 result_str.push_str(&val.to_string());
                 result_str.push_str(",")
             }
         }
-        runner::RunResultValue::Panic(values) => {
+        cairo_runner::RunResultValue::Panic(values) => {
             println!("Run panicked with err values: {values:?}")
         }
     }

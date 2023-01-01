474a8a9ea39590e2d11e91dfef261baa89a2aa4d
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-02 05:50:18 +0700

    Basic JS to render circles

--- static/script.js
+++ static/script.js
@@ -1,58 +1,72 @@
-function prepareAssetsArray() {
-  const assets = {
-    0: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/user.svg",
-    1: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/asterisk.svg",
-    2: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/times.svg",
-    3: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/circle-o.svg",
-    dot: "https://cdn.jsdelivr.net/npm/font-awesome-svg-icons@0.1.0/svg/dot-circle-o.svg",
+(() => {
+  const canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  const starterCode =
+    "// You can draw an arc or circle,\n// 'use' (equivalent to import) them from draw\nuse draw::circle;\nuse draw::arc;\n\n// Circle is also an arc\nuse draw::Arc;\n\n// Set return tuple with all shapes you are returning\nfn main() -> (Arc, Arc) {\n  (\n    circle( 384, 256, 100 ),\n    circle( 128, 256, 100 ),\n  )\n}";
+  document.querySelector('#cairo').innerHTML = starterCode;
+  window.submitCairo = async () => {
+    let cairo = document.getElementById('cairo').value;
+    let cairoResp = await fetch('./cairo', {
+      method: 'POST',
+      headers: {},
+      body: cairo,
+    });
+    cairoResp = await cairoResp.text();
+
+    document.getElementById('response').innerText = cairoResp;
+    processCairoResp(JSON.parse(cairoResp));
   };
 
-  const assetsArr = [];
-  for (const asset in assets) {
-    assetsArr.push({
-      id: asset,
-      src: assets[asset],
-      w: 70,
-      h: 70,
-    });
+  /*
+   * Converts felt to string
+   */
+  const feltToString = (felt) =>
+    felt
+      .toString(16) // To hex
+      .match(/.{2}/g) // Split into 2 chars
+      .map((c) => String.fromCharCode(parseInt(c, 16))) // Get char from code
+      .join(''); // Join to a string
+
+  const actionHandlers = {
+    arc(x, y, r, s, e) {
+      s = (2 * Math.PI * s) / 10000;
+      e = (2 * Math.PI * e) / 10000;
+      ctx.beginPath();
+      ctx.arc(x, y, r, s, e);
+      ctx.stroke();
+    },
+  };
+
+  function handleAction(action, args) {
+    action = feltToString(+action);
+    console.log(action, args);
+    if (typeof actionHandlers[action] === 'function') {
+      actionHandlers[action](...args);
+    } else if (typeof ctx[action] === 'function') {
+      ctx[action](...args);
+    } else if (typeof ctx[action] !== 'undefined' && args.length === 1) {
+      ctx[action] = args[0];
+    }
   }
 
-  return assetsArr;
-}
+  function processCairoResp(cairoResp) {
+    console.log(cairoResp);
+    let dt = cairoResp.replace(/,$/, '').split(',');
 
-async function setupDumbRenderer() {
-  const renderer = new DumbRenderer();
+    ctx.fillStyle = '#fcfaf6';
+    ctx.fillRect(0, 0, 9999, 9999);
+    ctx.fillStyle = '#fe4a49';
+    ctx.strokeStyle = '#fe4a49';
 
-  renderer.setupCanvas({
-    w: 512,
-    h: 512,
-    bg: "#fff",
-  });
+    callbacks = {};
 
-  // Wait for assets to load
-  await renderer.loadAssets(prepareAssetsArray());
-
-  return renderer;
-}
-
-window.submitCairo = async () => {
-  let cairo = document.getElementById("cairo").value;
-  let cairoResp = await fetch("./cairo", {
-    method: "POST",
-    headers: {},
-    body: cairo,
-  });
-  cairoResp = await cairoResp.text();
-
-  if (cairoResp.indexOf("Failed to") > -1) {
-    cairoResp = "Failed to compile.";
+    for (let i = 0; i < dt.length; i++) {
+      const action = dt[i];
+      const n_args = +dt[i + 1];
+      console.log(action, n_args);
+      const args = dt.slice(i + 2, i + 2 + n_args);
+      handleAction(action, args);
+      i += n_args + 1;
+    }
   }
-  document.getElementById("response").innerText = cairoResp;
-  console.log(cairoResp);
-};
-
-function ready(renderer) {
-  window.render = renderer;
-}
-
-setupDumbRenderer().then(ready);
+})();

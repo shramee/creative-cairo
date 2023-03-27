f7cee3d4993d48252ce38b99a1f0ec8a8dbaf60d
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-03-28 00:25:11 +0200

    Optional HTTPS (untested)

--- src/serve.rs
+++ src/serve.rs
@@ -5,6 +5,7 @@ use actix_cors::Cors;
 use actix_files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 // use clap::Parser;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 mod run_cairo;
 use run_cairo::run_cairo;
@@ -17,16 +18,30 @@ async fn main() -> std::io::Result<()> {
         Ok(val) => val.parse::<u16>().unwrap(),
         Err(_e) => 8080,
     };
+
     println!("Serving on port {}", port);
-    HttpServer::new(|| {
+    let server = HttpServer::new(|| {
         App::new()
             .wrap(Cors::permissive())
             .route("/cairo", web::post().to(handle_connection))
             .service(actix_files::Files::new("/", SERVE_PATH).show_files_listing())
-    })
-    .bind(("0.0.0.0", port))?
-    .run()
-    .await
+    });
+
+    match std::env::var("CREATIVE_CAIRO_SSL_KEY") {
+        Ok(ssl_key) => {
+            let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+            builder
+                .set_private_key_file(ssl_key, SslFiletype::PEM)
+                .unwrap();
+            builder.set_certificate_chain_file("cert.pem").unwrap();
+            server
+                .bind_openssl(("0.0.0.0", port), builder)
+                .unwrap()
+                .run()
+                .await
+        }
+        Err(_e) => server.bind(("0.0.0.0", port)).unwrap().run().await,
+    }
 }
 
 fn write_cairo_to_file(cairo: String) -> String {
@@ -45,6 +60,6 @@ fn handle_cairo_run(cairo: String) -> String {
 
 async fn handle_connection(req_body: String) -> impl Responder {
     let run_response = handle_cairo_run(req_body);
-    println!("{:#?}", run_response);
+    println!("{run_response:#?}");
     HttpResponse::Ok().body(run_response)
 }
